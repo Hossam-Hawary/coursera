cc4a6407f028cc8f9979800d4330d146429393d9
Hossam Hawary <Hossam0hawary@gmail.com>
2016-04-26 22:18:14 +0200

    set validation on profile_img

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   mount_uploader :profile_img, PictureUploader
+  validate  :profile_img_size
+  validates :name, :presence => true
+  validates :name,:uniqueness => true
+
+  private
+
+  # Validates the size of an uploaded picture.
+  def profile_img_size
+    if profile_img.size > 2.megabytes
+      errors.add(:profile_img, "should be less than 2MB")
+    end
+  end
 
 end
